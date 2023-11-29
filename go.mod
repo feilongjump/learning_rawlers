ebdaab54f3ee013a99e1f65484d6ebe096bfb231
ifeilong <feilongjump@qq.com>
2023-11-29 12:40:21 +0800

    save to excel

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/feilongjump/learning_rawlers
 
 go 1.21.3
 
-require github.com/gocolly/colly/v2 v2.1.0
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/xuri/excelize/v2 v2.8.0
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
@@ -14,8 +17,14 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/xuri/efp v0.0.0-20230802181842-ad255f2331ca // indirect
+	github.com/xuri/nfp v0.0.0-20230819163627-dc951e3ffe1a // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/xuri/excelize/v2"
 )
 
 // CrawlerItem 页面数据
@@ -18,8 +20,17 @@ type CrawlerItem struct {
 var CrawlerData = make(map[string]CrawlerItem)
 
 func main() {
-	// 访问的链接
+	// 页面的链接
 	url := ""
+	if err := crawler(url); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		saveExcel()
+	}
+}
+
+// crawler 爬取页面
+func crawler(url string) error {
 
 	c := colly.NewCollector()
 
@@ -28,16 +39,15 @@ func main() {
 		e.Request.Visit(e.Attr("href"))
 	})
 
+	// 限制数量访问
 	visitCount := 1
-	visitMaxCount := 2
+	visitMaxCount := 688
 	c.OnHTML(
 		"li.base-pagination__item.base-pagination__item--next.page-next > a.base-pagination__link",
 		func(e *colly.HTMLElement) {
 			link := e.Attr("href")
 			if link != "javascript:;" && visitCount <= visitMaxCount {
 				e.Request.Visit(link)
-
-				fmt.Println(visitCount)
 			}
 		},
 	)
@@ -50,23 +60,29 @@ func main() {
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Progress:", r.URL)
+		path := r.URL.Path
 		customURL := "/product_list" + strconv.Itoa(visitCount) + ".html"
-		if r.URL.Path == customURL || r.URL.Path == "/product.html" {
+		if path == customURL || path == "/product.html" {
 			visitCount++
 			fmt.Println("============= next page :", visitCount, "=============")
 			fmt.Println("============= 以下为当前页（", visitCount-1, "）的请求链接 =============")
-		}
-		// 赋值产品链接
-		link := r.URL.String()
-		if _, ok := CrawlerData[link]; !ok {
-			CrawlerData[link] = CrawlerItem{
-				Link: link,
+		} else {
+
+			// 赋值产品链接
+			link := r.URL.String()
+			if _, ok := CrawlerData[link]; !ok {
+				CrawlerData[link] = CrawlerItem{
+					Link: link,
+				}
 			}
 		}
 	})
-	c.Visit(url)
+	if err := c.Visit(url); err != nil {
+		log.Fatal(err)
+		return err
+	}
 
-	// fmt.Println(CrawlerData)
+	return nil
 }
 
 // setTitle 赋值产品标题
@@ -94,13 +110,53 @@ func setProductDetails(c *colly.Collector) {
 		// 获取产品详情是否存在
 		link := e.Request.URL.String()
 		if item, ok := CrawlerData[link]; ok {
-			item.ProductDetails = strings.TrimSpace(e.Text) == ""
+			item.ProductDetails = strings.TrimSpace(e.Text) != ""
 			CrawlerData[link] = item
 		} else {
 			CrawlerData[link] = CrawlerItem{
 				Link:           link,
-				ProductDetails: strings.TrimSpace(e.Text) == "",
+				ProductDetails: strings.TrimSpace(e.Text) != "",
 			}
 		}
 	})
+}
+
+// saveExcel 保存到 excel 中
+func saveExcel() {
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	// 创建一个工作表
+	index, err := f.NewSheet("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 设置列宽
+	f.SetColWidth("Sheet1", "A", "B", 120)
+	f.SetColWidth("Sheet1", "C", "C", 20)
+	// 设置单元格的值
+	cellNum := 2
+	f.SetCellValue("Sheet1", "A1", "产品链接")
+	f.SetCellValue("Sheet1", "B1", "产品标题")
+	f.SetCellValue("Sheet1", "C1", "是否存在产品详情")
+
+	for _, v := range CrawlerData {
+		cellNumStr := strconv.Itoa(cellNum)
+
+		f.SetCellValue("Sheet1", "A"+cellNumStr, strings.TrimSpace(v.Link))
+		f.SetCellValue("Sheet1", "B"+cellNumStr, strings.TrimSpace(v.Title))
+		f.SetCellValue("Sheet1", "C"+cellNumStr, v.ProductDetails)
+
+		cellNum++
+	}
+	// 设置工作簿的默认工作表
+	f.SetActiveSheet(index)
+	// 根据指定路径保存文件
+	if err := f.SaveAs("product.xlsx"); err != nil {
+		fmt.Println(err)
+	}
 }
